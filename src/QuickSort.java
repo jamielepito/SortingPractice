dbaa347d7749d3417a043a74cd7ddb939c01b34b
Jamie <lepitojg@beloit.edu>
2019-12-26 10:51:01 -0500

    Find the median of 3 for partition

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -17,5 +17,30 @@ public class QuickSort {
             array[i] = random.nextInt(10) + 1;
         }
         System.out.println(Arrays.toString(array));
+
+        //find first, mid, and last elements
+        int a = array[0];
+        int b = array[array.length/2];
+        int c = array[array.length-1];
+        //testing
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        //Find the Median
+        //median variables
+        int pivot;
+        int x = a-b; //(+ -> a>b) (- -> b>a)
+        int y = b-c; //(+ -> b>c) (- -> c>b)
+        int z = a-c; //(+ -> a>c) (- -> c>a)
+        //compare variables
+        if (x*y>0){
+            pivot = b; //a>b & b>c
+        }
+        else if (x*z>0){
+            pivot = c; //a>b & a>c & not b
+        }
+        else{ pivot = a;}
+        System.out.println("The median of 3 is " + pivot);
+
     }
 }
